f85bf5ca361f3faf8ba6ccadbb209bf1b756ddf9
weizhengda <2625785357@qq.com>
2018-03-22 11:23:46 +0800

    init

---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+   <div class="card">
+   </div>
+</template>
+<script>
+export default {
+  name: 'Card',
+  data () {
+    return {
+      msg: 'this is card'
+    }
+  }
+}
+</script>
+<style scoped>
+.card{
+    width:1280px;
+    height:692px;
+    margin:0 auto;
+    background:url(../assets/bg.png) no-repeat;
+    background-size: 1280px 692px;
+}
+
+</style>
+
